fe02bbecfe9a5402db4fdabdbe6d4a99f09620c0
Voggo <202106096@post.au.dk>
2023-12-04 17:16:56 +0100

    v2.0

---
+++ v2/client_new.py
@@ -0,0 +1,197 @@
+import socket
+import threading
+import time
+import pygame
+import random
+import sys
+import json
+
+
+# Buffer size for receiving the datagrams from server
+
+# Server IP address and Port number
+serverAddressPort = ("127.0.0.1", 8000)
+
+
+# Connect2Server forms the thread - for each connection made to the server
+def Connect2Server(game, metrics):
+    bufferSize = 1024
+    game_ele = None
+    ping = [-1]*100
+    last_time = 0
+    ping_index = 0
+    packet_id = 0
+    packets_delivered = [True]*1000
+    
+    # Create a socket instance - A datagram socket
+    UDPClientSocket = socket.socket(
+        family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    
+    msgToServer = f"{game.player_speed},{packet_id}"
+    UDPClientSocket.sendto(msgToServer.encode(), serverAddressPort)
+
+    # Send message to server using created UDP socket
+    while True:
+
+        # compute ping of last 100 packets
+        ping[ping_index] = time.time() - last_time
+        if ping_index == 99:
+            ping_index = 0
+        else:
+            ping_index += 1
+
+        data = UDPClientSocket.recvfrom(bufferSize)
+        game_ele = json.loads(data[0].decode())
+        game.game_ele = game_ele
+
+
+        # set packet as delivered
+        packets_delivered[game_ele['packet_id']] = True
+
+        # check packet packet loss
+        temp_lost_packets = 0
+        for id in packets_delivered:
+            if not id:
+                temp_lost_packets += 1
+        packet_loss = temp_lost_packets
+
+        msgToServer = f"{game.player_speed},{packet_id}"
+        UDPClientSocket.sendto(msgToServer.encode(), serverAddressPort)
+
+        last_time = time.time()
+
+        pygame.time.Clock().tick(60)
+
+
+class Game_elements:
+    game_ele = {}
+    player_speed = 0
+
+    def __init__(self):
+        self.game_ele['ball_speed_x'] = 7 + random.choice((1, -1))
+        self.game_ele['ball_speed_y'] = 7 + random.choice((1, -1))
+        self.game_ele['player_1_speed'] = 0
+        self.game_ele['player_2_speed'] = 0
+        self.game_ele['ball_x'] = 0
+        self.game_ele['ball_y'] = 0
+        self.game_ele['player_1_y'] = 0
+        self.game_ele['player_2_y'] = 0
+        self.game_ele['player_1_score'] = 0
+        self.game_ele['player_2_score'] = 0
+
+
+class Pong:
+    # Initialize Pygame
+    def __init__(self):
+        pygame.init() # pylint: disable=no-member
+
+        # Set up the game window
+        self.screen_width = 700
+        self.screen_height = 500
+        self.screen = pygame.display.set_mode(
+            (self.screen_width, self.screen_height))
+        pygame.display.set_caption("Pong")
+
+        # Define game colors
+        self.bg_color = pygame.Color("grey12")
+        self.light_grey = (200, 200, 200)
+
+        # Define game elements
+        self.ball = pygame.Rect(
+            self.screen_width/2 - 15, self.screen_height/2 - 15, 30, 30)
+        self.player = pygame.Rect(
+            self.screen_width - 20, self.screen_height/2 - 70, 10, 140)
+        self.opponent = pygame.Rect(10, self.screen_height/2 - 70, 10, 140)
+
+        self.game_font = pygame.font.Font("freesansbold.ttf", 32)
+
+        self.last_update = time.time()
+        self.update_rate = 0
+        self.hz = 0
+
+    def run(self, game, metrics):
+        game_ele = game.game_ele
+        # Handle events
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # pylint: disable=no-member
+                pygame.quit() # pylint: disable=no-member
+                sys.exit()
+            if event.type == pygame.KEYDOWN: # pylint: disable=no-member
+                if event.key == pygame.K_DOWN: # pylint: disable=no-member
+                    game.player_speed = 7
+                if event.key == pygame.K_UP: # pylint: disable=no-member
+                    game.player_speed = -7
+            if event.type == pygame.KEYUP: # pylint: disable=no-member
+                if event.key == pygame.K_DOWN: # pylint: disable=no-member
+                    game.player_speed = 0
+                if event.key == pygame.K_UP: # pylint: disable=no-member
+                    game.player_speed = 0
+
+        # Receive game state from server
+        self.ball.centerx = game_ele['ball_x']
+        self.ball.centery = game_ele['ball_y']
+        self.player.y = game_ele['player_1_y']
+        self.opponent.y =  game_ele['player_2_y']
+
+        # Draw game elements
+        self.screen.fill(self.bg_color)
+        pygame.draw.rect(self.screen, self.light_grey, self.player)
+        pygame.draw.rect(self.screen, self.light_grey, self.opponent)
+        pygame.draw.ellipse(self.screen, self.light_grey, self.ball)
+        pygame.draw.aaline(self.screen, self.light_grey, (self.screen_width /
+                           2, 0), (self.screen_width/2, self.screen_height))
+
+        # Draw scores
+        player_text = self.game_font.render(
+            f"{game_ele['player_2_score']}", False, self.light_grey)
+        self.screen.blit(player_text, (self.screen_width /
+                         2 + 20, self.screen_height/2 - 16))
+        opponent_text = self.game_font.render(
+            f"{game_ele['player_1_score']}", False, self.light_grey)
+        self.screen.blit(opponent_text, (self.screen_width /
+                         2 - 42, self.screen_height/2 - 16))
+        
+        # # update rate in hz
+        # self.hz = self.hz + 1
+        # if time.time() - self.last_update >= 1:
+        #     print(f"update rate: {self.hz}")
+        #     self.last_update = time.time()
+        #     self.update_rate = self.hz
+        #     self.hz = 0
+
+        # update_rate_text = self.game_font.render(
+        #     f"{self.update_rate} hz", False, self.light_grey)
+        # self.screen.blit(update_rate_text, (30, 50))
+        
+        # # Draw ping
+        # ping_list = [ping for ping in client.ping if ping != -1]
+        # avrg_ping = round(1000 * sum(ping_list)/len(ping_list), 3)
+        # ping_text = self.game_font.render(
+        #     f"{avrg_ping} ms", False, self.light_grey)
+        # self.screen.blit(ping_text, (30, 20))
+
+        # # draw lost packets
+        # lost_packets_text = self.game_font.render(f"{client.packet_loss} PL/s", False, self.light_grey)
+        # self.screen.blit(lost_packets_text, (30, 80))
+
+        # Update the display
+        pygame.display.flip()
+
+def loop(game, pong, metrics):
+    while True:
+        pong.run(game, metrics)
+        pygame.time.Clock().tick(60)
+
+if __name__ == "__main__":
+    print("Client - Main thread started")
+
+    game_ele = Game_elements()
+    pong = Pong()
+    metrics = {"packet_loss": 0, "ping": 0, "update_rate": 0}
+
+    ThreadInstance = threading.Thread(target=Connect2Server, args=(game_ele, metrics,))
+    ThreadInstance.daemon = True
+    ThreadInstance.start()
+
+    loop(game_ele, pong, metrics)
---
+++ v2/server_new.py
@@ -0,0 +1,130 @@
+import socketserver
+import threading
+import time
+import random
+import json
+
+import pygame
+
+# Create a tuple with IP Address and Port Number
+
+ServerAddress = ("127.0.0.1", 8000)
+
+# Subclass the DatagramRequestHandler
+
+class MyUDPRequestHandler(socketserver.DatagramRequestHandler):
+    clients = []
+    game = None
+
+    def handle(self):
+        print(type(self.client_address))
+        data = self.rfile.readline().strip()
+
+        if data.decode() == "STOP":
+            self.clients.remove(self.client_address)
+            self.game.reset_game()
+            return
+        if self.client_address not in self.clients:
+            self.clients.append(self.client_address)
+
+        data = (data.decode()).split(",")
+        if self.client_address == self.clients[0]:
+            self.game.game_ele['player_1_speed'] = int(data[0])
+        if len(self.clients) > 1 and self.client_address == self.clients[1]:
+            self.game.game_ele['player_2_speed'] = int(data[0])
+        self.game.game_ele['packet_id'] = int(data[1])
+
+        print(f"player 1 speed: {self.game.game_ele['player_1_speed']}")
+        print(f"player 2 speed: {self.game.game_ele['player_2_speed']}")
+
+        # Send game state back to client
+        game_state = json.dumps(self.game.game_ele)
+        # print(f"Sending: {game_state} to {addr}")
+        self.wfile.write(game_state.encode())
+
+
+class GameLogic:
+    screen_width = 700
+    screen_height = 500
+    game_ele = {}
+    game_ele['ball_speed_x'] = 0
+    game_ele['ball_speed_y'] = 0
+    game_ele['player_1_speed'] = 0
+    game_ele['player_2_speed'] = 0
+    game_ele['ball_x'] = screen_width/2
+    game_ele['ball_y'] = screen_height/2
+    game_ele['player_1_y'] = 0
+    game_ele['player_2_y'] = 0
+    game_ele['player_1_score'] = 0
+    game_ele['player_2_score'] = 0
+    game_ele['packet_id'] = 0
+
+    def reset_game(self):
+        self.game_ele['ball_speed_x'] = 0
+        self.game_ele['ball_speed_y'] = 0
+        self.game_ele['player_1_speed'] = 0
+        self.game_ele['player_2_speed'] = 0
+        self.game_ele['ball_x'] = self.screen_width/2
+        self.game_ele['ball_y'] = self.screen_height/2
+        self.game_ele['player_1_y'] = 0
+        self.game_ele['player_2_y'] = 0
+        self.game_ele['player_1_score'] = 0
+        self.game_ele['player_2_score'] = 0
+
+    def reset_ball(self):
+        self.game_ele['ball_x'] = self.screen_width/2
+        self.game_ele['ball_y'] = self.screen_height/2
+        self.game_ele['ball_speed_y'] = 0
+        self.game_ele['ball_speed_x'] = 0
+
+    def start_ball(self):
+        self.game_ele['ball_speed_y'] = 5 + random.choice((1, -1))
+        self.game_ele['ball_speed_x'] = 5 + random.choice((1, -1))
+
+def loop(game):
+    while True:
+        # Update game state
+        if game.game_ele['ball_speed_x'] == 0 and game.game_ele["player_1_speed"] != 0:
+            game.start_ball()
+        game.game_ele['ball_x'] = game.game_ele['ball_x'] + \
+            game.game_ele['ball_speed_x']
+        game.game_ele['ball_y'] = game.game_ele['ball_y'] + \
+            game.game_ele['ball_speed_y']
+
+        if game.game_ele['ball_y'] + 15 <= 0 or game.game_ele['ball_y'] + 15 >= game.screen_height:
+            game.game_ele['ball_speed_y'] *= -1
+        if game.game_ele['ball_x'] <= 0:
+            print("Player 2 scored!")
+            game.game_ele['player_2_score'] += 1
+            game.reset_ball()
+        if game.game_ele['ball_x'] >= game.screen_width:
+            print(f"ball x: {game.game_ele['ball_x']}")
+            print("Player 1 scored!")
+            game.game_ele['player_1_score'] += 1
+            game.reset_ball()
+        if game.game_ele['ball_x'] >= game.screen_width - 38 and game.game_ele['ball_y'] >= game.game_ele['player_1_y'] and game.game_ele['ball_y'] <= game.game_ele['player_1_y'] + 140 and game.game_ele['ball_speed_x'] > 0:
+            game.game_ele['ball_speed_x'] *= -1
+        if game.game_ele['ball_x'] <= 38 and game.game_ele['ball_y'] >= game.game_ele['player_2_y'] and game.game_ele['ball_y'] <= game.game_ele['player_2_y'] + 140 and game.game_ele['ball_speed_x'] < 0:
+            game.game_ele['ball_speed_x'] *= -1
+
+        # Move paddles
+        game.game_ele['player_1_y'] += game.game_ele['player_1_speed']
+        game.game_ele['player_2_y'] += game.game_ele['player_2_speed']
+
+        pygame.time.Clock().tick(60)
+
+
+if __name__ == "__main__":
+    game = GameLogic()
+
+    # Create a Server Instance
+    t = threading.Thread(target=loop, args=(game,))
+    t.daemon = True
+    t.start()
+
+    MyUDPRequestHandler.game = game
+    UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress, MyUDPRequestHandler)
+
+    # Make the server wait forever serving connections
+
+    UDPServerObject.serve_forever()
